8fa4485993c4db89483463858cb486dcf0afd60d
Philip Degarmo <aclysma@gmail.com>
2020-08-16 10:05:24 -0700

    Add mouse/keyboard events and the breakout sample

---
+++ examples/breakout.rs
@@ -0,0 +1,266 @@
+use bevy::{
+    prelude::*,
+    render::pass::ClearColor,
+    sprite::collide_aabb::{collide, Collision},
+};
+
+/// An implementation of the classic game "Breakout"
+fn main() {
+    App::build()
+        .add_default_plugins()
+        .add_plugin(bevy_sdl2::Sdl2Plugin::default())
+        .add_resource(Scoreboard { score: 0 })
+        .add_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
+        .add_startup_system(setup.system())
+        .add_system(paddle_movement_system.system())
+        .add_system(ball_collision_system.system())
+        .add_system(ball_movement_system.system())
+        .add_system(scoreboard_system.system())
+        .run();
+}
+
+struct Paddle {
+    speed: f32,
+}
+
+struct Ball {
+    velocity: Vec3,
+}
+
+struct Scoreboard {
+    score: usize,
+}
+
+struct Brick;
+struct Wall;
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Add the game's entities to our world
+    commands
+        // cameras
+        .spawn(Camera2dComponents::default())
+        .spawn(UiCameraComponents::default())
+        // paddle
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+            translation: Translation(Vec3::new(0.0, -250.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(120.0, 30.0),
+            },
+            ..Default::default()
+        })
+        .with(Paddle { speed: 500.0 })
+        // ball
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.8, 0.2, 0.2).into()),
+            translation: Translation(Vec3::new(0.0, -100.0, 1.0)),
+            sprite: Sprite {
+                size: Vec2::new(30.0, 30.0),
+            },
+            ..Default::default()
+        })
+        .with(Ball {
+            velocity: 400.0 * Vec3::new(0.5, -0.5, 0.0).normalize(),
+        })
+        // scoreboard
+        .spawn(TextComponents {
+            text: Text {
+                font: asset_server.load("assets/fonts/FiraSans-Bold.ttf").unwrap(),
+                value: "Score:".to_string(),
+                style: TextStyle {
+                    color: Color::rgb(0.2, 0.2, 0.8).into(),
+                    font_size: 40.0,
+                },
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    top: Val::Px(5.0),
+                    left: Val::Px(5.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+
+    // Add walls
+    let wall_material = materials.add(Color::rgb(0.5, 0.5, 0.5).into());
+    let wall_thickness = 10.0;
+    let bounds = Vec2::new(900.0, 600.0);
+
+    commands
+        // left
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(-bounds.x() / 2.0, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(wall_thickness, bounds.y() + wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Wall)
+        // right
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(bounds.x() / 2.0, 0.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(wall_thickness, bounds.y() + wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Wall)
+        // bottom
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(0.0, -bounds.y() / 2.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(bounds.x() + wall_thickness, wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Wall)
+        // top
+        .spawn(SpriteComponents {
+            material: wall_material,
+            translation: Translation(Vec3::new(0.0, bounds.y() / 2.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(bounds.x() + wall_thickness, wall_thickness),
+            },
+            ..Default::default()
+        })
+        .with(Wall);
+
+    // Add bricks
+    let brick_rows = 4;
+    let brick_columns = 5;
+    let brick_spacing = 20.0;
+    let brick_size = Vec2::new(150.0, 30.0);
+    let bricks_width = brick_columns as f32 * (brick_size.x() + brick_spacing) - brick_spacing;
+    // center the bricks and move them up a bit
+    let bricks_offset = Vec3::new(-(bricks_width - brick_size.x()) / 2.0, 100.0, 0.0);
+
+    for row in 0..brick_rows {
+        let y_position = row as f32 * (brick_size.y() + brick_spacing);
+        for column in 0..brick_columns {
+            let brick_position = Vec3::new(
+                column as f32 * (brick_size.x() + brick_spacing),
+                y_position,
+                0.0,
+            ) + bricks_offset;
+            commands
+                // brick
+                .spawn(SpriteComponents {
+                    material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+                    sprite: Sprite { size: brick_size },
+                    translation: Translation(brick_position),
+                    ..Default::default()
+                })
+                .with(Brick);
+        }
+    }
+}
+
+fn paddle_movement_system(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Paddle, &mut Translation)>,
+) {
+    for (paddle, mut translation) in &mut query.iter() {
+        let mut direction = 0.0;
+        if keyboard_input.pressed(KeyCode::Left) {
+            direction -= 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) {
+            direction += 1.0;
+        }
+
+        *translation.0.x_mut() += time.delta_seconds * direction * paddle.speed;
+    }
+}
+
+fn ball_movement_system(time: Res<Time>, mut ball_query: Query<(&Ball, &mut Translation)>) {
+    for (ball, mut translation) in &mut ball_query.iter() {
+        translation.0 += ball.velocity * time.delta_seconds;
+    }
+}
+
+fn scoreboard_system(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
+    for mut text in &mut query.iter() {
+        text.value = format!("Score: {}", scoreboard.score);
+    }
+}
+
+fn ball_collision_system(
+    mut commands: Commands,
+    mut scoreboard: ResMut<Scoreboard>,
+    mut ball_query: Query<(&mut Ball, &Translation, &Sprite)>,
+    mut paddle_query: Query<(&Paddle, &Translation, &Sprite)>,
+    mut brick_query: Query<(Entity, &Brick, &Translation, &Sprite)>,
+    mut wall_query: Query<(&Wall, &Translation, &Sprite)>,
+) {
+    for (mut ball, ball_translation, sprite) in &mut ball_query.iter() {
+        let ball_size = sprite.size;
+        let velocity = &mut ball.velocity;
+        let mut collision = None;
+
+        // check collision with walls
+        for (_wall, translation, sprite) in &mut wall_query.iter() {
+            if collision.is_some() {
+                break;
+            }
+
+            collision = collide(ball_translation.0, ball_size, translation.0, sprite.size);
+        }
+
+        // check collision with paddle(s)
+        for (_paddle, translation, sprite) in &mut paddle_query.iter() {
+            if collision.is_some() {
+                break;
+            }
+
+            collision = collide(ball_translation.0, ball_size, translation.0, sprite.size);
+        }
+
+        // check collision with bricks
+        for (brick_entity, _brick, translation, sprite) in &mut brick_query.iter() {
+            if collision.is_some() {
+                break;
+            }
+
+            collision = collide(ball_translation.0, ball_size, translation.0, sprite.size);
+            if collision.is_some() {
+                scoreboard.score += 1;
+                commands.despawn(brick_entity);
+            }
+        }
+
+        // reflect the ball when it collides
+        let mut reflect_x = false;
+        let mut reflect_y = false;
+
+        // only reflect if the ball's velocity is going in the opposite direction of the collision
+        match collision {
+            Some(Collision::Left) => reflect_x = velocity.x() > 0.0,
+            Some(Collision::Right) => reflect_x = velocity.x() < 0.0,
+            Some(Collision::Top) => reflect_y = velocity.y() < 0.0,
+            Some(Collision::Bottom) => reflect_y = velocity.y() > 0.0,
+            None => {}
+        }
+
+        // reflect velocity on the x-axis if we hit something on the x-axis
+        if reflect_x {
+            *velocity.x_mut() = -velocity.x();
+        }
+
+        // reflect velocity on the y-axis if we hit something on the y-axis
+        if reflect_y {
+            *velocity.y_mut() = -velocity.y();
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-
+mod converters;
 use bevy_app::prelude::*;
-use bevy_window::{CreateWindow, Windows, WindowCreated, Window, WindowCloseRequested};
+use bevy_window::{CreateWindow, Windows, WindowCreated, WindowCloseRequested, WindowResized, CursorMoved};
 use bevy_app::AppExit;
 use bevy_ecs::Resources;
 use crate::sdl2_windows::Sdl2Windows;
 use bevy_input::keyboard::{KeyboardInput, ElementState};
+use bevy_math::prelude::*;
+use bevy_input::mouse::{MouseMotion, MouseButtonInput};
 
 mod sdl2_windows;
 
@@ -44,6 +46,9 @@ pub fn sdl2_runner(mut app: App) {
         &mut create_window_event_reader
     );
 
+    // SDL2 has a lot of events we can support, expose them all via an event
+    app.resources.insert(Events::<sdl2::event::Event>::default());
+
     log::debug!("Entering SDL2 event loop");
 
     'running: loop {
@@ -53,67 +58,108 @@ pub fn sdl2_runner(mut app: App) {
             }
         }
 
-        for event in event_pump.poll_iter() {
-            match event {
-                // sdl2::event::Event::Quit => {
-                //     let mut window_close_requested_events = app
-                //         .resources
-                //         .get_mut::<Events<WindowCloseRequested>>()
-                //         .unwrap();
-                //     let winit_windows = app.resources.get_mut::<WinitWindows>().unwrap();
-                //     let window_id = winit_windows.get_window_id(winit_window_id).unwrap();
-                //     window_close_requested_events.send(WindowCloseRequested { id: window_id });
-                // },
-                sdl2::event::Event::Window {
-                    timestamp,
-                    window_id,
-                    win_event
-                } => {
-                    match win_event {
-                        sdl2::event::WindowEvent::Close => {
-                            let mut window_close_requested_events = app
-                                .resources
-                                .get_mut::<Events<WindowCloseRequested>>()
-                                .unwrap();
-                            let window_id = sdl2_windows.get_window_id(window_id).unwrap();
-                            window_close_requested_events.send(WindowCloseRequested { id: window_id });
-                        },
-                        _ => {}
-                    }
-                },
-                sdl2::event::Event::KeyDown {
-                    timestamp,
-                    window_id,
-                    keycode,
-                    scancode,
-                    keymod,
-                    repeat
-                } => {
-                    let mut keyboard_input_events =
-                        app.resources.get_mut::<Events<KeyboardInput>>().unwrap();
-                    keyboard_input_events.send(KeyboardInput {
-                        scan_code: 0,
-                        key_code: 0,
-                        state: ElementState::Pressed
-                    });
-                },
-                sdl2::event::Event::KeyDown {
-                    timestamp,
-                    window_id,
-                    keycode,
-                    scancode,
-                    keymod,
-                    repeat
-                } => {
-                    let mut keyboard_input_events =
-                        app.resources.get_mut::<Events<KeyboardInput>>().unwrap();
-                    keyboard_input_events.send(KeyboardInput {
-                        scan_code: 0,
-                        key_code: 0,
-                        state: ElementState::Released
-                    });
+        {
+            let mut sdl2_events = app.resources.get_mut::<Events<sdl2::event::Event>>().unwrap();
+            for event in event_pump.poll_iter() {
+                match event {
+                    sdl2::event::Event::Window {
+                        window_id,
+                        win_event,
+                        ..
+                    } => {
+                        match win_event {
+                            sdl2::event::WindowEvent::Close => {
+                                let mut window_close_requested_events = app
+                                    .resources
+                                    .get_mut::<Events<WindowCloseRequested>>()
+                                    .unwrap();
+                                let window_id = sdl2_windows.get_window_id(window_id).unwrap();
+                                window_close_requested_events.send(WindowCloseRequested { id: window_id });
+                            },
+                            sdl2::event::WindowEvent::Resized(width, height) => {
+                                let mut windows = app.resources.get_mut::<Windows>().unwrap();
+                                let bevy_window_id = sdl2_windows.get_window_id(window_id).unwrap();
+                                let mut window = windows.get_mut(bevy_window_id).unwrap();
+                                window.width = width as u32;
+                                window.height = height as u32;
+
+                                let mut resize_events = app.resources.get_mut::<Events<WindowResized>>().unwrap();
+                                resize_events.send(WindowResized {
+                                    id: bevy_window_id,
+                                    height: window.height as usize,
+                                    width: window.width as usize,
+                                });
+                            }
+                            _ => {}
+                        }
+                    },
+                    sdl2::event::Event::KeyDown {
+                        keycode,
+                        scancode,
+                        ..
+                    } => {
+                        send_key_event(
+                            &app,
+                            keycode,
+                            scancode,
+                            ElementState::Pressed
+                        );
+                    },
+                    sdl2::event::Event::KeyUp {
+                        keycode,
+                        scancode,
+                        ..
+                    } => {
+                        send_key_event(
+                            &app,
+                            keycode,
+                            scancode,
+                            ElementState::Released
+                        );
+                    },
+                    sdl2::event::Event::MouseMotion {
+                        window_id,
+                        x,
+                        y,
+                        xrel,
+                        yrel,
+                        ..
+                    } => {
+                        let mut cursor_moved_events =
+                            app.resources.get_mut::<Events<CursorMoved>>().unwrap();
+
+                        let bevy_window_id = sdl2_windows.get_window_id(window_id).unwrap();
+                        let window = sdl2_windows.get_window(bevy_window_id).unwrap();
+                        let (_width, height) = window.size();
+                        // move origin to bottom left
+                        let y_position = height as i32 - y;
+                        cursor_moved_events.send(CursorMoved {
+                            id: bevy_window_id,
+                            position: Vec2::new(x as f32, y_position as f32)
+                        });
+
+                        let mut mouse_motion_events =
+                            app.resources.get_mut::<Events<MouseMotion>>().unwrap();
+                        mouse_motion_events.send(MouseMotion {
+                            delta: Vec2::new(xrel as f32, yrel as f32),
+                        });
+                    },
+                    sdl2::event::Event::MouseButtonDown {
+                        mouse_btn,
+                        ..
+                    } => {
+                        send_mouse_event(&app, mouse_btn, bevy_input::keyboard::ElementState::Pressed);
+                    },
+                    sdl2::event::Event::MouseButtonUp {
+                        mouse_btn,
+                        ..
+                    } => {
+                        send_mouse_event(&app, mouse_btn, bevy_input::keyboard::ElementState::Released);
+                    },
+                    _ => {}
                 }
-                _ => {}
+
+                sdl2_events.send(event);
             }
         }
 
@@ -125,6 +171,44 @@ pub fn sdl2_runner(mut app: App) {
         );
         app.update();
     }
+}
+
+fn send_key_event(
+    app: &App,
+    keycode: Option<sdl2::keyboard::Keycode>,
+    scancode: Option<sdl2::keyboard::Scancode>,
+    element_state: bevy_input::keyboard::ElementState,
+) {
+    let mut keyboard_input_events =
+        app.resources.get_mut::<Events<KeyboardInput>>().unwrap();
+
+    // These options are due to conversion from C types to i32.
+    // - Keycode is typedeffed as i32
+    // - Scancode is a C array with max value of 512. It is intended that end-users
+    //   can make arrays of this length.
+    //
+    // So it is expected that the i32 conversion will always succeed and these unwrap
+    // are infallible
+    let keycode = keycode.unwrap();
+    let scancode = scancode.unwrap();
+    keyboard_input_events.send(converters::convert_keyboard_input(
+        keycode,
+        scancode,
+        element_state
+    ));
+}
+
+fn send_mouse_event(
+    app: &App,
+    mouse_btn: sdl2::mouse::MouseButton,
+    element_state: bevy_input::keyboard::ElementState,
+) {
+    let mut mouse_button_input_events =
+        app.resources.get_mut::<Events<MouseButtonInput>>().unwrap();
+    mouse_button_input_events.send(MouseButtonInput {
+        button: converters::convert_mouse_button(mouse_btn),
+        state: element_state,
+    });
 }
 
 fn handle_create_window_events(
